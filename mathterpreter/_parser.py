6bc6df3ee4836cc11230e201ec05281d5d113867
Cytamir Lobaker <lobakerstudio@live.ru>
2021-03-22 14:27:14 +0600

    Decimals and exponent

--- mathterpreter/_parser.py
+++ mathterpreter/_parser.py
@@ -3,95 +3,97 @@ from mathterpreter.tokens import TokenType, Token
 from mathterpreter.exceptions import MathSyntaxError
 from typing import List
 
+
 class Parser:
     def __init__(self, tokens: List[Token]):
-        self.__tokens = iter(tokens)
+        self._tokens = iter(tokens)
         self.token_list = tokens
-        self.__token = None
-        self.__index = -1
-        self.__iterate_token__()
+        self._token = None
+        self._index = -1
+        self._iterate_token()
 
-    def __iterate_token__(self):
+    def _iterate_token(self):
         try:
-            self.__token = next(self.__tokens)
-            self.__index += 1
+            self._token = next(self._tokens)
+            self._index += 1
         except StopIteration:
-            self.__token = None
+            self._token = None
 
     def parse(self):
-        if self.__token is None:
+        if self._token is None:
             return None
 
-        result = self.__addition_subtraction_base__()
+        result = self._addition_subtraction_base()
 
         return result
 
-    def __addition_subtraction_base__(self):
-        if self.__token is None:
+    def _addition_subtraction_base(self):
+        if self._token is None:
             output = ''.join([z.__str__() for z in self.token_list])
             raise MathSyntaxError("Expression expected", f"{output}\n{'^^^'.rjust(len(output) + 3)}")
-        result = self.__multiplication_division__()
-        while self.__token is not None:
-            if self.__token.type == TokenType.ADDITION_OPERATOR:
-                self.__iterate_token__()
-                result = AdditionNode(result, self.__multiplication_division__())
-            elif self.__token.type == TokenType.SUBTRACTION_OPERATOR:
-                self.__iterate_token__()
-                result = SubtractionNode(result, self.__multiplication_division__())
+        result = self._multiplication_division()
+        while self._token is not None:
+            if self._token.type == TokenType.ADDITION_OPERATOR:
+                self._iterate_token()
+                result = AdditionNode(result, self._multiplication_division())
+            elif self._token.type == TokenType.SUBTRACTION_OPERATOR:
+                self._iterate_token()
+                result = SubtractionNode(result, self._multiplication_division())
             else:
                 break
 
         return result
 
-    def __multiplication_division__(self):
-        result = self.__exponentiation_root__()
+    def _multiplication_division(self):
+        result = self._exponentiation_root()
 
-        while self.__token is not None:
-            if self.__token.type == TokenType.MULTIPLICATION_OPERATOR:
-                self.__iterate_token__()
-                result = MultiplicationNode(result, self.__exponentiation_root__())
-            elif self.__token.type == TokenType.DIVISION_OPERATOR:
-                self.__iterate_token__()
-                result = DivisionNode(result, self.__exponentiation_root__())
-            elif self.__token.type == TokenType.OPENING_BRACKET:
-                result = MultiplicationNode(result, self.__literal_polarity__())
+        while self._token is not None:
+            if self._token.type == TokenType.MULTIPLICATION_OPERATOR:
+                self._iterate_token()
+                result = MultiplicationNode(result, self._exponentiation_root())
+            elif self._token.type == TokenType.DIVISION_OPERATOR:
+                self._iterate_token()
+                result = DivisionNode(result, self._exponentiation_root())
+            elif self._token.type == TokenType.OPENING_BRACKET:
+                result = MultiplicationNode(result, self._literal_polarity())
             else:
                 break
 
         return result
 
-    def __exponentiation_root__(self):
-        result = self.__literal_polarity__()
-        while self.__token is not None:
-            if self.__token.type == TokenType.POWER_OPERATOR:
-                self.__iterate_token__()
-                result = ExponentiationNode(result, self.__literal_polarity__())
-            elif self.__token.type == TokenType.SQRT_OPERATOR:
-                self.__iterate_token__()
-                result = RootNode(result, self.__literal_polarity__())
+    def _exponentiation_root(self):
+        result = self._literal_polarity()
+        while self._token is not None:
+            if self._token.type == TokenType.POWER_OPERATOR:
+                self._iterate_token()
+                result = ExponentiationNode(result, self._literal_polarity())
+            elif self._token.type == TokenType.SQRT_OPERATOR:
+                self._iterate_token()
+                result = RootNode(result, self._literal_polarity())
             else:
                 break
         return result
 
-    def __literal_polarity__(self):
-        token = self.__token
+    def _literal_polarity(self):
+        token = self._token
         if token.type == TokenType.OPENING_BRACKET:
-            self.__iterate_token__()
-            result = self.__addition_subtraction_base__()
-            self.__iterate_token__()
+            self._iterate_token()
+            result = self._addition_subtraction_base()
+            self._iterate_token()
             return result
 
         else:
-            self.__iterate_token__()
+            self._iterate_token()
             if token.type == TokenType.NUMBER:
                 return NumberNode(token.value)
 
             elif token.type == TokenType.ADDITION_OPERATOR:
-                return PositiveNode(self.__literal_polarity__())
+                return PositiveNode(self._literal_polarity())
 
             elif token.type == TokenType.SUBTRACTION_OPERATOR:
-                return NegativeNode(self.__literal_polarity__())
+                return NegativeNode(self._literal_polarity())
             else:
                 output = [z.__str__() for z in self.token_list]
                 raise MathSyntaxError("Unexpected token",
-                                      f"{''.join(output)}\n{'^'.rjust(sum([len(z) for z in output[:self.__index]]))}")
+                                      f"{''.join(output)}\n"
+                                      f"{'^'.rjust(sum([len(z) for z in output[:self._index]]))}")
